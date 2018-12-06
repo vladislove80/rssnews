00cc8d889273d003006b6f3652b10e8406706ca8
vladislove80 <vladislove.n.u@gmail.com>
2018-12-06 17:40:36 +0200

    added placeholder

--- app/src/main/java/com/rssnews/ua/fragment/NewsHolder.kt
+++ app/src/main/java/com/rssnews/ua/fragment/NewsHolder.kt
@@ -2,6 +2,9 @@ package com.rssnews.ua.fragment
 
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.rssnews.R
 import com.rssnews.data.model.NewsItem
 import com.rssnews.ua.fragment.base.BaseHolder
@@ -13,10 +16,21 @@ import kotlinx.android.synthetic.main.news_row.*
 
 class NewsHolder(recyclerView: ViewGroup) : BaseHolder<NewsItem>(recyclerView, R.layout.news_row, intArrayOf()) {
     override fun bindItem(model: NewsItem) {
+        val subtitle = model.author + " " + model.date
+        val apply = RequestOptions().apply {
+            placeholder(R.drawable.placeholder)
+            error(R.drawable.placeholder)
+            diskCacheStrategy(DiskCacheStrategy.ALL)
+            priority(Priority.HIGH)
+        }
         tvTitle.text = model.title
-        val s = model.author + "" + model.date
-        tvAuthorAndDate.text = s
-        Glide.with(context).load(model.imageLink).into(iv)
+        tvAuthorAndDate.text = subtitle
+        iv.apply {
+            Glide.with(context)
+                .load(model.imageLink)
+                .apply(apply)
+                .into(this)
+        }
         tvImageDescription.text = model.imageDescription
         tvDescription.text = model.description
     }
