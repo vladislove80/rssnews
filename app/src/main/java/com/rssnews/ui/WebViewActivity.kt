4644416dae8f084346a88ab0a1b5c5c7f5524315
vladislove80 <vladislove.n.u@gmail.com>
2018-12-07 15:51:33 +0200

    added vwb view cache

--- app/src/main/java/com/rssnews/ui/WebViewActivity.kt
+++ app/src/main/java/com/rssnews/ui/WebViewActivity.kt
@@ -3,6 +3,7 @@ package com.rssnews.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebSettings
 import androidx.appcompat.app.AppCompatActivity
 import com.rssnews.R
 import kotlinx.android.synthetic.main.activity_webview.*
@@ -25,11 +26,22 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
         overridePendingTransition(R.anim.trans_left_in, R.anim.trans_left_out)
+        enableWebViewCache()
         webView.loadUrl(intent.getStringExtra(INTENT_KEY))
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.trans_right_in, R.anim.trans_right_out);
+    }
+
+    //todo enable/disable by settings
+    private fun enableWebViewCache() {
+        webView.apply {
+            settings.domStorageEnabled = true
+            settings.setAppCachePath(cacheDir.absolutePath)
+            settings.setAppCacheEnabled(true)
+            settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+        }
     }
 }
